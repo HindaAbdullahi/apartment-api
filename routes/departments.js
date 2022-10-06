549f7fdb05cb16da41a3b262247bc094a30d14c9
Hinda-Abdullahi <99586014+HindaAbdullahi@users.noreply.github.com>
2022-10-06 14:14:56 +0300

    Update departments.js

--- routes/departments.js
+++ routes/departments.js
@@ -3,53 +3,60 @@ const auth = require("../middleware/auth");
 const validateObjectID = require("../middleware/objectdId");
 const validator = require("../middleware/validate");
 const { validate, Department } = require("../models/department");
+const singleDepartment = async (req, res) => {
 
-const router = express.Router();
+    const dept = await Department.find({});
+    res.send(dept);
+};
+const getDepartments = async (req, res) => {
+    const { id } = req.params
 
-router.get("/", async (req, res) => {
-  const dept = await Department.find({});
-  res.send(dept);
-});
 
-router.post("/", [validator(validate)], async (req, res) => {
-  let dept = await Department.findOne({ name: req.body.name });
-  if (dept) return res.status(400).send("department already registered");
+    const dep = await Department.findById(id)
+    Department(dep)
 
-  dept = await Department.create({
-    name: req.body.name,
-    manager: req.body.manager,
-    phone: req.body.phone,
-  });
+    res.status(200).json(dep)
+}
+const createDepartment = async (req, res) => {
+    let dept = await Department.findOne({ name: req.body.name });
+    if (dept) return res.status(400).send("department already registered");
 
-  res.send(dept);
-});
+    dept = await Department.create({
+        name: req.body.name,
+        manager: req.body.manager,
+        phone: req.body.phone,
+    });
 
-router.put(
-  "/:id",
-  [validateObjectID, validator(validate)],
-  async (req, res) => {
+    res.send(dept);
+}
+const updateDepartment = async (req, res) => {
     let dept = await Department.findOne({ name: req.body.name });
     if (dept) return res.status(400).send("department already registered");
 
     dept = await Department.findByIdAndUpdate(
-      req.params.id,
-      {
-        name: req.body.name,
-        manager: req.body.manager,
-        phone: req.body.phone,
-      },
-      { new: true }
+        req.params.id,
+        {
+            name: req.body.name,
+            manager: req.body.manager,
+            phone: req.body.phone,
+        },
+        { new: true }
     );
 
     res.send(dept);
-  }
-);
+}
 
-router.delete("/:id", [validateObjectID], async (req, res) => {
-  const dept = await Department.findByIdAndRemove(req.params.id);
-  if (!dept) return res.status(404).send("invalid delete");
+const deleteDepartment = async (req, res) => {
 
-  res.send(dept);
-});
+    const dept = await Department.findByIdAndRemove(req.params.id);
+    if (!dept) return res.status(404).send("invalid delete");
 
-module.exports = router;
+    res.send(dept);
+}
+module.exports = {
+    singleDepartment,
+    getDepartments,
+    createDepartment,
+    updateDepartment,
+    deleteDepartment,
+};
